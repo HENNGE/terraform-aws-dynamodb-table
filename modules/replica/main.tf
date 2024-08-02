9ba92574a22cf40b0cb97c745bd89eeb875f74e9
Muhammad Furqan Habibi <furqan.habibi@hennge.com>
2024-08-02 15:14:58 +0900

    Always create autoscaling target

--- modules/replica/main.tf
+++ modules/replica/main.tf
@@ -13,9 +13,8 @@ module "replica_autoscaling" {
 
   count = var.autoscaling_enabled ? 1 : 0
 
-  table_name                = split("/", aws_dynamodb_table_replica.replica.global_table_arn)[1]
-  create_autoscaling_target = true
-  autoscaling_read          = merge({ min_capacity = var.read_capacity }, var.autoscaling_read)
-  autoscaling_write         = merge({ min_capacity = var.write_capacity }, var.autoscaling_write)
-  autoscaling_indexes       = var.autoscaling_indexes
+  table_name          = split("/", aws_dynamodb_table_replica.replica.global_table_arn)[1]
+  autoscaling_read    = merge({ min_capacity = var.read_capacity }, var.autoscaling_read)
+  autoscaling_write   = merge({ min_capacity = var.write_capacity }, var.autoscaling_write)
+  autoscaling_indexes = var.autoscaling_indexes
 }
